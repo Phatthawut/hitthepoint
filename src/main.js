55abd0c921b6800add636b9c4ad5df25acd2412d
Phatthawut Chaiyapin <phatthawut.cnx@gmail.com>
2025-04-22 22:46:50 +0700

    fix-main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
-import { VueReCaptcha } from "vue-recaptcha-v3";
 
 const app = createApp(App);
 
 // Create and use Pinia store
 const pinia = createPinia();
 app.use(pinia);
-
-// Configure and use reCAPTCHA
-app.use(VueReCaptcha, {
-  siteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
-  loaderOptions: {
-    useRecaptchaNet: true,
-    autoHideBadge: false,
-  },
-});
-
 app.use(router);
 app.mount("#app");
